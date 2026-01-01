4d7d77e5edf73d19ff5aa6ee35ec8302e168416b
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2026-01-01 23:43:41 +0600

    script.js updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,4 +73,35 @@ window.addEventListener("scroll", function () {
     } else {
         header.classList.remove("active");
     }
-})
+});
+
+/* 
+
+===================== HERO SLLIDER =====================
+
+*/
+
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+const heroSliderNextBtn = document.querySelector("[data-next-btn]");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSliderPos = function () {
+    lastActiveSliderItem.classList.remove("active");
+    heroSliderItems[currentSlidePos].classList.add("active");
+    lastActiveSliderItem = heroSliderItems[currentSlidePos];
+}
+
+const slideNext = function () {
+
+    if(currentSlidePos >= heroSliderItems.length - 1) {
+        currentSlidePos = 0;
+    } else {
+        currentSlidePos++;
+    }
+
+    updateSliderPos();
+}
